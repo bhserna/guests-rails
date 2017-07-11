5b4c15bbef80a7b363286974b10e51f49c5283d9
bhserna <bhserna@gmail.com>
2017-07-11 18:40:16 -0500

    mark invitation as delivered

---
+++ app/controllers/delivery_marks_controller.rb
@@ -0,0 +1,21 @@
+class DeliveryMarksController < ApplicationController
+  def create
+    Lists.mark_invitation_as_delivered(invitation_id, ListInvitationRecord)
+    redirect_to list_path(list_id)
+  end
+
+  def destroy
+    Lists.mark_invitation_as_not_delivered(invitation_id, ListInvitationRecord)
+    redirect_to list_path(list_id)
+  end
+
+  private
+
+  def list_id
+    params[:list_id]
+  end
+
+  def invitation_id
+    params[:invitation_id]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :registrations, only: [:new, :create]
   resources :lists, only: [:new, :create, :index, :show] do
-    resources :invitations, only: [:create]
+    resources :invitations, only: [:create] do
+      resource :delivery_mark, only: [:create, :destroy]
+    end
   end
 end
