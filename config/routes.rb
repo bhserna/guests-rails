dd7ef76ab2743ca8421ace82918e52a13ed5bdde
bhserna <bhserna@gmail.com>
2017-07-25 20:43:36 -0500

    add support button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "pages#index"
   resource :session, only: [:new, :create, :destroy]
   resources :registrations, only: [:new, :create]
+  resource :page, only: [] do
+    get :support
+  end
   resources :lists, only: [:new, :create, :index, :show] do
     resources :invitations, only: [:create, :edit, :update, :destroy] do
       resource :delivery_mark, only: [:create, :destroy]
