476b0717b6696fbec6050147e2d4914dcee8da3b
bhserna <bhserna@gmail.com>
2017-08-05 13:56:04 -0500

    remove default scope

--- app/models/list_invitation_record.rb
+++ app/models/list_invitation_record.rb
@@ -1,15 +1,13 @@
 class ListInvitationRecord < ApplicationRecord
-  default_scope { order(:id) }
-
   def self.find_all_by_list_id(list_id)
-    where(list_id: list_id)
+    where(list_id: list_id).order(:id)
   end
 
   def self.find_all_groups_by_list_id(list_id)
-    where(list_id: list_id).where.not(group: nil).pluck(:group).uniq
+    order(:id).where(list_id: list_id).where.not(group: nil).pluck(:group).uniq
   end
 
   def self.counts_by_list_id
-    group(:list_id).distinct.count(:id)
+    select(:id, :list_id).group(:list_id).count(:id)
   end
 end
