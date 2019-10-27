3785720f990eee18b23db188fb9328654badef3d
bhserna <bhserna@gmail.com>
2019-10-27 12:40:30 -0600

    Change default url options to invita.app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: "simpleguestlist.bhserna.com"
+    host: "invita.app"
   }
 
   config.action_mailer.delivery_method = :smtp
